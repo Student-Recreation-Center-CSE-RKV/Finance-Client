4f5718a6a7e62b72cdcc6cc89f23db0e385fd59f
revanthkumarJ <jrevanth101@gmail.com>
2024-12-27 14:27:18 +0530

    Delete Route Integaration

---
+++ src/components/FeeInstallmentTable.js
@@ -0,0 +1,86 @@
+import React from 'react';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+} from "@mui/material";
+import {CustomTableCell} from './DueDetails';
+
+const FeeInstallmentsTable = ({ title, installmentsData, feeType }) => {
+  const feeRows = ["Receipt No", "Amount", "Date"];
+
+  return (
+    <TableContainer
+      component={Paper}
+      sx={{
+        width: "100%",
+        margin: "auto",
+      }}
+    >
+      <Table>
+        <TableHead>
+          <TableRow sx={{ backgroundColor: "#000" }}>
+            <TableCell
+              colSpan={6}
+              sx={{
+                fontWeight: "bold",
+                textAlign: "center",
+                color: "#fff",
+                border: "1px solid black",
+              }}
+            >
+              {title}
+            </TableCell>
+          </TableRow>
+        </TableHead>
+        <TableHead>
+          <TableRow>
+            {feeRows.map((row) => (
+              <CustomTableCell key={row} data={row} />
+            ))}
+            {feeRows.map((row) => (
+              <CustomTableCell key={row} data={row} />
+            ))}
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {installmentsData?.map((item, index) => {
+            if (index % 2 === 0) {
+              // Pair the current item with the next item in the row
+              const nextItem = installmentsData[index + 1];
+              return (
+                <TableRow key={index}>
+                  <TableCell align="center" sx={{ border: "1px solid black" }}>
+                    {item.ReceiptNo}
+                  </TableCell>
+                  <TableCell align="center" sx={{ border: "1px solid black" }}>
+                    {item.Amount}
+                  </TableCell>
+                  <TableCell align="center" sx={{ border: "1px solid black" }}>
+                    {item.Date}
+                  </TableCell>
+                  <TableCell align="center" sx={{ border: "1px solid black" }}>
+                    {nextItem ? nextItem.ReceiptNo : ""}
+                  </TableCell>
+                  <TableCell align="center" sx={{ border: "1px solid black" }}>
+                    {nextItem ? nextItem.Amount : ""}
+                  </TableCell>
+                  <TableCell align="center" sx={{ border: "1px solid black" }}>
+                    {nextItem ? nextItem.Date : ""}
+                  </TableCell>
+                </TableRow>
+              );
+            }
+            return null;
+          })}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+};
+
+export default FeeInstallmentsTable;
